e1759ef4c5069bfbb069011348adc09e26863c92
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-04-02 11:35:58 -0300

    css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -46,11 +46,12 @@ function App() {
                   <div className="product-description-container-right">
                     <div className="product-title">Title</div>
                     <div className="product-description">
-                      O colchão mais ergonômico e suave, combinação de espumas
+                     O colchão mais ergonômico e suave, combinação de espumas
                       para um descanso reparador. O colchão mais ergonômico e
                       suave, combinação de espumas para um descanso reparador. O
                       colchão mais ergonômico e suave, combinação de espumas
                       para um descanso reparador.
+                      
                     </div>
                     <div className="btn-info">
                       <button className="ver-oferta">VER OFERTA</button>
@@ -80,11 +81,7 @@ function App() {
                     <div className="product-description-container-left">
                       <div className="product-title">Title</div>
                       <div className="product-description">
-                        O colchão mais ergonômico e suave, combinação de espumas
-                        para um descanso reparador. O colchão mais ergonômico e
-                        suave, combinação de espumas para um descanso reparador.
-                        O colchão mais ergonômico e suave, combinação de espumas
-                        para um descanso reparador.
+                      O colchão mais ergonômico e suave, combinação de espumas para um descanso reparador. O colchão mais ergonômico e suave, combinação de espumas para um descanso reparador. O colchão mais ergonômico e suave, combinação de espumas para um descanso reparador.
                       </div>
                       <div className="btn-info">
                         <button className="ver-oferta">VER OFERTA</button>
@@ -332,7 +329,7 @@ function App() {
               <div className="footer-content">
                 <div className="footer-title">Localização</div>
                 <div className="footer-content-inner">
-                
+                  <div className='icon-container-inner'>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -352,6 +349,7 @@ function App() {
                       d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
                     />
                   </svg>
+                  </div>
                   <div className='address'>
                     Av. do Estado Dalmo Vieira, 570 Ariribá, Balneário
                     Camboriú-SC
@@ -362,6 +360,7 @@ function App() {
               <div className="footer-content">
                 <div className="footer-title">Contatos</div>
                 <div className="footer-content-inner">
+                <div className='icon-container-inner'>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -376,7 +375,8 @@ function App() {
                       d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
                     />
                   </svg>
-                  <p>47 xxxxx-xxxx</p>
+                  </div>
+                  <div className='phone'>47 55555-5555 </div>
                 </div>
               </div>
             </div>
