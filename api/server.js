7d5f67d952c5e552957cae57860a79974546e24d
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-04-13 17:24:43 -0300

    nodemon

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'http://localhost:4000']
 }));
 
 app.use(morgan('dev'));
