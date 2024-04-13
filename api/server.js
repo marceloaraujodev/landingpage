6a290b38547ec8b13731d442a5ad53cdf98a0bc1
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-04-13 17:43:03 -0300

    cors

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-  origin: ["https://landingpage-server-2k7v.onrender.com", 'http://localhost:5173', 'http://localhost:4000', ]
+  origin: ["https://landingpage-dn42.onrender.com", 'http://localhost:5173', 'http://localhost:4000', ]
 }));
 
 app.use(morgan('dev'));
@@ -38,7 +38,6 @@ app.get('/', async (req, res) => {
   } catch (error) {
     console.log(error)
   }
-
 
 
 })
